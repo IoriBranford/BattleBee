5428c072db5c73212540ab750cc2899c692825f4
IoriBranford <ioribranford@gmail.com>
2017-02-07 12:59:13 -0800

    Player control.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer _instance;
+
+	void Awake () {
+		if (_instance == null) {
+			GameObject.DontDestroyOnLoad(gameObject);
+			_instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	// Between Awake and Start is when an audio source starts playing
+
+	void Start () {
+	}
+
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 inputDir = new Vector2 (Input.GetAxisRaw("Horizontal"), 0);
+		var rigidBody = GetComponent<Rigidbody2D> ();
+		rigidBody.velocity = inputDir * speed;
+	}
+}
