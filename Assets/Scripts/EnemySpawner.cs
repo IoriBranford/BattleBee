f104d9c203a8369385aefc7f79dfbdb27a94fee4
IoriBranford <ioribranford@gmail.com>
2017-02-20 14:29:17 -0800

    Enemy formations with movement.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+
+	private float _ampX;
+	private float _freqX;
+	private float _time;
+
+	void OnDrawGizmos () {
+		Gizmos.DrawWireCube(transform.position,
+				new Vector3 (width, height));
+	}
 
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate (enemyPrefab,
-			new Vector3(0, 20, 0), Quaternion.identity);
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate (enemyPrefab,
+					child.transform.position,
+					Quaternion.identity);
+			enemy.transform.SetParent(child);
+		}
+		_time = 0;
 
-		enemy.transform.SetParent (transform);
+		Vector3 cameraBL = Camera.main.ViewportToWorldPoint
+			(new Vector3 (0, 0, 0));
+		Vector3 cameraCenter = Camera.main.ViewportToWorldPoint
+			(new Vector3 (.5f, .5f, 0));
+
+		_ampX = (cameraCenter - cameraBL).x - width*.5f;
+		_freqX = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 position = transform.position;
+		_time += Time.deltaTime;
+		position.x = _ampX * Mathf.Sin(_time * _freqX);
+		transform.position = position;
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 1;
-	private float _xMin = -10, _xMax = 10;
+	private Bounds _bounds;
 
 	// Use this for initialization
 	void Start () {
-		_xMin = Camera.main.ViewportToWorldPoint
-			(new Vector3 (0, 0, 0)).x;
-		_xMax = Camera.main.ViewportToWorldPoint
-			(new Vector3 (1, 0, 0)).x;
+		Vector3 cameraBL = Camera.main.ViewportToWorldPoint
+			(new Vector3 (0, 0, 0));
+		Vector3 cameraTR = Camera.main.ViewportToWorldPoint
+			(new Vector3 (1, 1, 0));
+		Vector3 cameraCenter = Camera.main.ViewportToWorldPoint
+			(new Vector3 (.5f, .5f, 0));
 
-		Collider2D collider = GetComponent<Collider2D> ();
-		if (collider) {
+		_bounds = new Bounds(cameraCenter, cameraTR - cameraBL);
+
+		Vector2 min = _bounds.min;
+		Vector2 max = _bounds.max;
+		foreach (var collider in GetComponents<Collider2D>()) {
 			Bounds bounds = collider.bounds;
-			_xMin += bounds.extents.x;
-			_xMax -= bounds.extents.x;
+			min.x = Mathf.Max(min.x, cameraBL.x + bounds.extents.x);
+			min.y = Mathf.Max(min.y, cameraBL.y + bounds.extents.y);
+			max.x = Mathf.Min(max.x, cameraTR.x - bounds.extents.x);
+			max.y = Mathf.Min(max.y, cameraTR.y - bounds.extents.y);
 		}
+		_bounds.min = min;
+		_bounds.max = max;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 position = transform.position;
-		position.x += Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-		position.x = Mathf.Clamp (position.x, _xMin, _xMax);
+		float frameSpeed = speed * Time.deltaTime;
+		position.x += Input.GetAxisRaw("Horizontal") * frameSpeed;
+		position.y += Input.GetAxisRaw("Vertical") * frameSpeed;
+		position.x = Mathf.Clamp (position.x,
+				_bounds.min.x, _bounds.max.x);
+		position.y = Mathf.Clamp (position.y,
+				_bounds.min.y, _bounds.max.y);
 		transform.position = position;
 	}
 }
