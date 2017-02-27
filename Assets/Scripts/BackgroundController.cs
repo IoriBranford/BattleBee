0f2c56b261f65bbbb91fdb812c2805515be07c2d
IoriBranford <ioribranford@gmail.com>
2017-02-27 11:57:35 -0800

    Background sways horizontally with player.

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour {
+	public float speedY = 1f;
+
+	private float _maxOffsetX;
+
 	// Use this for initialization
 	void Start () {
+		var camera = Camera.main;
+		Vector3 cameraBL = Camera.main.ViewportToWorldPoint
+			(new Vector3 (0, 0, 0));
+		Vector3 cameraTR = Camera.main.ViewportToWorldPoint
+			(new Vector3 (1, 1, 0));
+
+		float cameraW = cameraTR.x - cameraBL.x;
+		float width = transform.localScale.x;
+		_maxOffsetX = 1f - (cameraW / width);
 	}
 
 	// Update is called once per frame
@@ -13,7 +26,16 @@ public class BackgroundController : MonoBehaviour {
 		Material material = renderer.material;
 		Vector2 offset = material.mainTextureOffset;
 
-		offset.y += Time.deltaTime;
+		var player = GameObject.FindObjectOfType<PlayerController>();
+		var camera = Camera.main;
+		if (player && camera) {
+			Vector3 playerVPPos = camera.WorldToViewportPoint(
+					player.transform.position);
+
+			offset.x = playerVPPos.x * _maxOffsetX;
+		}
+
+		offset.y += Time.deltaTime * speedY;
 
 		material.mainTextureOffset = offset;
 	}
