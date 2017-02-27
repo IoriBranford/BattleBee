1cba73bd1fe16c7deadee07553a754351b4b0cc2
IoriBranford <ioribranford@gmail.com>
2017-02-27 11:05:13 -0800

    Scrolling background. Point filtering on textures as they are all pixel art.

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var renderer = GetComponent<MeshRenderer>();
+		Material material = renderer.material;
+		Vector2 offset = material.mainTextureOffset;
+
+		offset.y += Time.deltaTime;
+
+		material.mainTextureOffset = offset;
+	}
+}
